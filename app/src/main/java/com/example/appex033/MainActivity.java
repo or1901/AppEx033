b834285af4b66761840f8f8f80568be122498fe5
orira22 <oriroitzaid@gmail.com>
2022-12-22 16:06:41 +0200

    Final commit - a button that counts the clicks on it, and when the number of clicks is 6 it returns to count from the beginning

--- app/src/main/java/com/example/appex033/MainActivity.java
+++ app/src/main/java/com/example/appex033/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.appex033;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int clicks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        clicks = 0;
     }
-}
+
+    public void countClicks(View view) {
+        clicks++;
+
+        if(clicks != 6)
+            btn.setText("This is a click number: " + clicks);
+        else{
+            btn.setText("Enough to click. Go to new start!");
+            clicks = 0;
+        }
+
+        }
+    }
